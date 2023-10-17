d0dbb8f3881458438804a2dea0e76e80f07655ca
Surendar PD <80924716+surendar-pd@users.noreply.github.com>
2023-10-17 18:44:52 +0530

    added more doodles

--- components/home/Stories.tsx
+++ components/home/Stories.tsx
@@ -1,44 +1,79 @@
-import { User } from 'lucide-react';
-import React from 'react';
+import { User } from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
 const Stories = () => {
-	return (
-		<section className='w-full h-full bg-white py-16 lg:py-32'>
-			<div className='h-full flex flex-col items-start gap-8 justify-center p-4 md:px-16 lg:max-w-7xl lg:mx-auto text-center'>
-				<h2 className='text-2xl lg:text-4xl leading-tight mx-auto '>
-					Stories
-				</h2>
-				<p className='text-base lg:text-xl text-slate-500'>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-					Suspendisse varius enim in eros elementum tristique. Duis
-					cursus, mi quis viverra ornare
-				</p>
-				<ul className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
-					{Array(5)
-						.fill({
-							person: 'Jacob Pierce',
-							story: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare',
-						})
-						.map((story, index) => (
-							<li
-								key={`story-${index}`}
-								className='bg-app-bg rounded-md flex flex-col items-center p-4 gap-4'
-							>
-								<div className='mt-4 rounded-md overflow-hidden bg-white p-4'>
-									<User size={44} strokeWidth={1.5} />
-								</div>
-								<p className='font-medium text-xl '>
-									{story.person}
-								</p>
-								<p className='text-base lg:text-lg text-slate-500'>
-									&quot;{story.story}&quot;
-								</p>
-							</li>
-						))}
-				</ul>
-			</div>
-		</section>
-	);
+    return (
+        <section className="w-full h-full bg-white py-16 lg:py-32 relative">
+            <Image
+                src={"/doodles/flower.svg"}
+                unoptimized
+                width={100}
+                height={100}
+                className="w-8 md:w-16 lg:w-28 object-contain absolute bottom-0 right-0 lg:right-10 h-auto"
+                alt={"flower"}
+            />
+            <Image
+                src={"/doodles/star2.svg"}
+                unoptimized
+                width={100}
+                height={100}
+                className="w-12 object-contain absolute bottom-4 lg:bottom-12 left-0 md:left-4 lg:left-16 h-auto"
+                alt={"star2"}
+            />
+            <div className="h-full flex flex-col items-start gap-8 justify-center p-4 md:px-16 lg:max-w-7xl lg:mx-auto text-center">
+                <div className="relative">
+                    <Image
+                        src={"/doodles/star2.svg"}
+                        unoptimized
+                        width={100}
+                        height={100}
+                        className="w-16 object-contain absolute -top-12 right-0 md:-right-6 lg:-top-24 h-auto"
+                        alt={"star2"}
+                    />
+                    <Image
+                        src={"/doodles/star.svg"}
+                        unoptimized
+                        width={100}
+                        height={100}
+                        className="w-20 md:w-24 lg:w-28 object-contain absolute -top-16 rotate-90 left-0 md:-left-6 lg:-top-24 h-auto"
+                        alt={"star"}
+                    />
+                    <h2 className="text-2xl lg:text-4xl leading-tight mx-auto ">
+                        Stories
+                    </h2>
+                    <p className="text-base lg:text-xl text-slate-500">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Suspendisse varius enim in eros elementum tristique.
+                        Duis cursus, mi quis viverra ornare
+                    </p>
+                </div>
+                <ul className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+                    {Array(5)
+                        .fill({
+                            person: "Jacob Pierce",
+                            story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare",
+                        })
+                        .map((story, index) => (
+                            <li
+                                key={`story-${index}`}
+                                className="bg-app-bg rounded-md flex flex-col items-center p-4 gap-4"
+                            >
+                                <div className="mt-4 rounded-md overflow-hidden bg-white p-4">
+                                    <User size={44} strokeWidth={1.5} />
+                                </div>
+                                <p className="font-medium text-xl ">
+                                    {story.person}
+                                </p>
+                                <p className="text-base lg:text-lg text-slate-500">
+                                    &quot;{story.story}&quot;
+                                </p>
+                            </li>
+                        ))}
+                </ul>
+            </div>
+        </section>
+    );
 };
 
 export default Stories;
--- components/home/WhatWeDo.tsx
+++ components/home/WhatWeDo.tsx
@@ -3,7 +3,15 @@ import React from "react";
 
 const WhatWeDo = () => {
     return (
-        <section className="w-full h-full bg-white">
+        <section className="w-full h-full relative bg-white">
+            <Image
+                src={"/doodles/star2.svg"}
+                unoptimized
+                width={100}
+                height={100}
+                className="w-12 lg:w-16 object-contain right-8 lg:right-12 top-12 absolute h-auto"
+                alt={"star2"}
+            />
             <div className="py-16 lg:py-32 flex flex-col justify-center lg:flex-row gap-6 p-4 md:px-16 lg:max-w-7xl lg:mx-auto">
                 <div className="flex flex-col gap-4 lg:w-1/2">
                     <h2 className="text-2xl lg:text-4xl">What we do</h2>
@@ -14,7 +22,15 @@ const WhatWeDo = () => {
                         cumque perspiciatis culpa eaque voluptates dicta modi
                         voluptatibus!
                     </p>
-                    <div className="bg-app-secondary/50 text-black rounded-lg p-4 flex items-center justify-between w-full md:mx-auto lg:mx-0 md:w-fit">
+                    <div className="bg-app-secondary/50 relative text-black rounded-lg p-4 flex items-center justify-between w-full md:mx-auto lg:mx-0 md:w-fit">
+                        <Image
+                            src={"/doodles/arrow.svg"}
+                            unoptimized
+                            width={100}
+                            height={100}
+                            className="w-20 lg:w-28 object-contain absolute bottom-8 lg:-bottom-20 hidden md:block -left-24 -rotate-[120deg] lg:rotate-180 h-auto"
+                            alt={"arrow"}
+                        />
                         <div className="w-1/4">
                             <Image
                                 src={
