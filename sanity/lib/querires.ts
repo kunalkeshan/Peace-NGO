373ef222472bd87098e6ae9ae04ea474b0a65c04
kunalkeshan <kunalkeshan12@gmail.com>
2023-10-27 20:40:08 +0530

    chore: updated schemas and queries
    Co-authored-by: surendar pd <surendarpd007@gmail.com>

--- sanity/lib/querires.ts
+++ sanity/lib/querires.ts
@@ -2,12 +2,12 @@ import { groq } from 'next-sanity';
 
 // Get all posts
 export const postsQuery = groq`*[_type == "post" && defined(slug.current)]{
-    _id, title, slug
+    _id, title, slug, "image": mainImage.asset->url, "alt": mainImage.alt, body, author, categories, publishedAt
   }`;
 
 // Get a single post by its slug
 export const postQuery = groq`*[_type == "post" && slug.current == $slug][0]{ 
-    title, mainImage, body
+    _id, slug, title, "image": mainImage.asset->url, "alt": mainImage.alt, body, author, categories, publishedAt
   }`;
 
 // Get all post slugs
--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,10 +1,13 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from 'sanity';
 
-import blockContent from './schemas/blockContent'
-import category from './schemas/category'
-import post from './schemas/post'
-import author from './schemas/author'
+import blockContent from './schemas/blockContent';
+import category from './schemas/category';
+import post from './schemas/post';
+import team from './schemas/team';
+import story from './schemas/story';
+import impact from './schemas/impact';
+import gallery from './schemas/gallery';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent],
-}
+	types: [post, team, category, blockContent, story, impact, gallery],
+};
