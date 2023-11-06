f5c8e1775af0a54bd5b15fe7c2d3e47d490f821f
kunalkeshan <kunalkeshan12@gmail.com>
2023-11-06 18:13:33 +0530

    chore: synchronized gallery image items

---
+++ components/cards/GalleryImage.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ImageListItem from '@mui/material/ImageListItem';
+import Image from 'next/image';
+
+type GalleryImageProps = React.ComponentProps<'li'> & {
+	image: GalleryImage;
+};
+
+const GalleryImageItem: React.FC<GalleryImageProps> = ({ image }) => {
+	return (
+		<ImageListItem>
+			<div>
+				<Image
+					unoptimized
+					loading='lazy'
+					src={image?.image ?? ''}
+					className='w-full rounded-lg border-b border-black'
+					width={10}
+					height={10}
+					alt={image?.alt}
+				/>
+			</div>
+			<p className='text-center'>{image.caption}</p>
+		</ImageListItem>
+	);
+};
+
+export default GalleryImageItem;
