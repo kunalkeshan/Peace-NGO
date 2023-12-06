3d33c9eda312e1815145c3910a7eba9ac836194b
Kunal Keshan <kunalkeshan12@gmail.com>
2023-12-07 02:56:09 +0530

    Updated Nav Flow with Data (#31)
    * chore: updated navigation data
    * chore: footer nav using navigation data
    * chore: added ngo data constants
    * chore: mapping out socials data

--- layouts/Footer.tsx
+++ layouts/Footer.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import { Instagram, Linkedin, Mail, Twitter } from 'lucide-react';
+import { FOOTER_NAVIGATION } from '@/data/navigation';
+import { PEACE_NGO_SOCIALS } from '@/data/ngo';
 
 const Footer = () => {
 	return (
@@ -21,82 +22,38 @@ const Footer = () => {
 								</Link>
 							</Link>
 							<div className='flex w-full flex-col items-center justify-between md:flex-row lg:justify-end'>
-								<div className='mb-4 flex flex-col items-stretch justify-start text-center md:flex-row'>
-									{/* <a className='p-3 text-[14px] md:px-3 md:py-4 lg:mx-3 lg:py-2 '>
-										How we work
-									</a>
-									<a className='p-3 text-[14px] md:px-3 md:py-4 lg:mx-3 lg:py-2 '>
-										Services
-									</a>
-									<a className='p-3 text-[14px] md:px-3 md:py-4 lg:mx-3 lg:py-2 '>
-										Solution
-									</a>
-									<a className='p-3 text-[14px] md:px-3 md:py-4 lg:mx-3 lg:py-2 '>
-										FAQs
-									</a> */}
-								</div>
 								<div className='mb-4 flex items-center justify-start lg:ml-[64px]'>
-									<a
-										target='_blank'
-										rel='noopener noreferrer'
-										href='https://www.linkedin.com/company/codelance-devs/'
-										className='mx-[2px] p-3 transition-all duration-300 hover:text-app-secondary'
-									>
-										<Linkedin />
-									</a>
-									<a
-										target='_blank'
-										rel='noopener noreferrer'
-										href='https://twitter.com/CodelanceDevs'
-										className='mx-[2px] p-3 transition-all duration-300 hover:text-app-secondary'
-									>
-										<Twitter />
-									</a>
-									<a
-										target='_blank'
-										rel='noopener noreferrer'
-										href='https://www.instagram.com/codelancedevs/'
-										className='mx-[2px] p-3 transition-all duration-300 hover:text-app-secondary'
-									>
-										<Instagram />
-									</a>
-									<a
-										target='_blank'
-										rel='noopener noreferrer'
-										href='mailto:contact@codelancedevs.com'
-										className='mx-[2px] p-3 transition-all duration-300 hover:text-app-secondary'
-									>
-										<Mail />
-									</a>
+									{PEACE_NGO_SOCIALS.map((social) => (
+										<Link
+											key={`footer-social-${social.name}`}
+											target='_blank'
+											rel='noopener noreferrer'
+											href={social.url}
+											title={social.name}
+											className='mx-[2px] p-3 transition-all duration-300 hover:text-app-secondary'
+										>
+											<social.Icon />
+										</Link>
+									))}
 								</div>
 							</div>
 						</div>
 					</div>
 					<div className='w-full p-2 md:p-4'>
 						<div className='flex flex-col items-center justify-start md:flex-row md:justify-between'>
-							<div className='mb-4 flex flex-col items-stretch justify-center text-center md:flex-row'>
-								<Link
-									href='/terms-and-conditions'
-									className='py-2 text-[14px] text-[#777a85] md:mr-[24px]'
-								>
-									Terms and Conditions
-								</Link>
-								<Link
-									href='/privacy-policy'
-									className='py-2 text-[14px] text-[#777a85] md:mr-[24px]'
-								>
-									Privacy Policy
-								</Link>
-								<a
-									target='_blank'
-									rel='noopener noreferrer'
-									href='https://blog.codelancedevs.com'
-									className='py-2 text-[14px] text-[#777a85] md:mr-[24px]'
-								>
-									Blog
-								</a>
+							<div className='mb-4 flex flex-wrap items-stretch justify-center text-center gap-8'>
+								{FOOTER_NAVIGATION.map((link) => (
+									<Link
+										key={`footer-link-${link.url}`}
+										href={link.url}
+										target={link.target}
+										className='py-2 text-[14px] text-slate-500 hover:text-slate-800 transition-all duration-300'
+									>
+										{link.name}
+									</Link>
+								))}
 							</div>
-							<p className='mb-4 text-[12px] text-[#777a85]'>
+							<p className='mb-4 text-[12px] text-slate-500'>
 								Copyright &copy; 2023 Peace NGO
 							</p>
 						</div>
