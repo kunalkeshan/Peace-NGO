393fc194f0e5c5bc2797b948529466fbb1c3070a
kunalkeshan <kunalkeshan12@gmail.com>
2023-12-07 02:49:09 +0530

    chore: added ngo data constants

---
+++ data/ngo.ts
@@ -0,0 +1,35 @@
+// Peace NGO Constants
+
+// Dependencies
+import { Linkedin, Instagram, LucideIcon, X, Mail } from 'lucide-react';
+
+type SocialsList = ReadonlyArray<{
+	name: string;
+	url: string;
+	Icon: LucideIcon;
+}>;
+
+export const PEACE_NGO_EMAIL = '' as const;
+
+export const TPH_SOCIALS: SocialsList = [
+	{
+		name: 'Instagram',
+		url: 'https://www.instagram.com/',
+		Icon: Instagram,
+	},
+	{
+		name: 'X',
+		url: 'https://twitter.com/',
+		Icon: X,
+	},
+	{
+		name: 'LinkedIn',
+		url: 'https://www.instagram.com/',
+		Icon: Linkedin,
+	},
+	{
+		name: 'Mail',
+		url: `mailto:${PEACE_NGO_EMAIL}`,
+		Icon: Mail,
+	},
+];
