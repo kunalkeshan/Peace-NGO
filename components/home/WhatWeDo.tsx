0b12b1d46c7bc89a1e6ba8075d7154c8d74d9090
kunalkeshan <kunalkeshan12@gmail.com>
2023-12-20 17:04:28 +0530

    chore: added impact conent

--- components/home/WhatWeDo.tsx
+++ components/home/WhatWeDo.tsx
@@ -37,11 +37,8 @@ const WhatWeDo = () => {
 						<span className='text-app-secondary'>What</span> we do
 					</h2>
 					<p className='text-base lg:text-lg text-slate-500'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Libero consectetur veritatis explicabo quas quae.
-						Laborum, tempore ullam impedit quia omnis sit, excepturi
-						cumque perspiciatis culpa eaque voluptates dicta modi
-						voluptatibus!
+						3 years. Thousands of People. Diverse Village
+						communities impacted.
 					</p>
 					<div className='bg-app-secondary/50 relative text-black rounded-lg p-4 flex items-center justify-between w-full md:mx-auto lg:mx-0 md:w-fit'>
 						<Image
@@ -52,21 +49,10 @@ const WhatWeDo = () => {
 							className='w-20 lg:w-28 object-contain absolute bottom-8 lg:-bottom-20 hidden md:block -left-24 -rotate-[120deg] lg:rotate-180 h-auto'
 							alt={'arrow'}
 						/>
-						<div className='w-1/4'>
-							<Image
-								src={
-									'https://free4kwallpapers.com/uploads/originals/2020/05/29/-peaceful-pier-wallpaper.jpg'
-								}
-								unoptimized
-								width={100}
-								height={100}
-								className='w-12 rounded-xl object-cover h-12'
-								alt={'image'}
-							/>
-						</div>
-						<div className='w-3/4'>
+						<div className='w-full'>
 							<p className='text-sm lg:text-base'>
-								&quot;Make big change and help the world.&quot;
+								&quot;Creating a Thriving Tomorrow, One Rural
+								Youth at a Time&quot;
 							</p>
 						</div>
 					</div>
