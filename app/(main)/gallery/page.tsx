1d404a238ea28f83dc8e39bfd05d6d7b42d9eb7a
Kunal Keshan <kunalkeshan12@gmail.com>
2024-01-14 20:17:41 +0530

    fix: added blur image loading for gallery images

--- app/(main)/gallery/page.tsx
+++ app/(main)/gallery/page.tsx
@@ -8,18 +8,30 @@ import { galleryImagesQuery } from '@/sanity/lib/querires';
 import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import GalleryImageItem from '@/components/cards/GalleryImage';
+import { dynamicBlurDataUrl } from '@/lib/dynamicBlurDataUrl';
+
+type GImages = Array<GalleryImage & { blurDataURL: string }>;
 
 export default async function Gallery() {
-	const galleryImages = await sanityFetch<GalleryImage[]>({
+	const galleryImages = await sanityFetch<GImages>({
 		query: galleryImagesQuery,
 	});
+
+	const images = await Promise.all(
+		galleryImages.map(async (image) => {
+			const blurDataURL = await dynamicBlurDataUrl(image?.image);
+			image.blurDataURL = blurDataURL;
+			return image;
+		})
+	);
+
 	return (
 		<main className=''>
 			<div className='w-full lg:max-w-7xl lg:mx-auto gap-4 p-4 md:px-16 py-[40px] md:py-[80px] lg:py-[100px]'>
 				<div className='md:hidden'>
 					<Box sx={{ width: '100%', height: '100%' }}>
 						<ImageList variant='masonry' cols={1} gap={8}>
-							{galleryImages.map((item) => (
+							{images.map((item) => (
 								<GalleryImageItem
 									key={`item-list-1-${item._id}`}
 									image={item}
@@ -31,7 +43,7 @@ export default async function Gallery() {
 				<div className='hidden md:block lg:hidden'>
 					<Box sx={{ width: '100%', height: '100%' }}>
 						<ImageList variant='masonry' cols={2} gap={8}>
-							{galleryImages.map((item) => (
+							{images.map((item) => (
 								<GalleryImageItem
 									key={`item-list-2-${item._id}`}
 									image={item}
@@ -43,7 +55,7 @@ export default async function Gallery() {
 				<div className='hidden lg:block'>
 					<Box sx={{ width: '100%', height: '100%' }}>
 						<ImageList variant='masonry' cols={3} gap={8}>
-							{galleryImages.map((item) => (
+							{images.map((item) => (
 								<GalleryImageItem
 									key={`item-list-3-${item._id}`}
 									image={item}
